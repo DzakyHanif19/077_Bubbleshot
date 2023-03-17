12131fcac6053d67089e0122d86c35b452d20e19
DzakyHanif19 <dzakyannafi1@gmail.com>
2023-03-17 16:41:17 +0700

    menambah input

--- Bubbleshot/Bubbleshot.cpp
+++ Bubbleshot/Bubbleshot.cpp
@@ -2,4 +2,25 @@
 using namespace std;
 
 int a[20];           // Deklarasi array a dengan ukuran 20
-int n;               // Deklarasi variabel n untuk menyimpan banyaknya elemen pada array 
+int n;               // Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+
+void input() {        // Method unutk input 
+	while (true) {   // Looping 
+		cout << "Masukan banyaknya elemen pada array : ";  // output ke layar 
+		cin >> n;     // input dari pengguna 
+		if (n <= 20)  // Jika n kurang dari atau sama dengan 20
+			break;    // Keluar dari loop 
+		else {        // Jika n lebih dari 20
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // output ke layar 
+		}
+	}
+	cout << endl;                            // Output ke baris kosong 
+	cout << "===================" << endl;   // Output ke layar 
+	cout << "Masukan Elemen Array" << endl;  // Output ke layar 
+	cout << "===================" << endl;   // Output ke layar 
+
+	for (int i = 0; i < n; i++) {   // Looping dengan i dimulai dari  0 hingga n-1
+		cout << "Data ke-" << (i + 1) << ": ";   // Output ke layar 
+		cin >> a[i];                    // Input dari pengguna 
+	}
+}
